851571fc9a68f5cf7af109e47b30b7b6a8eca05c
wilkanowski <wilkanowski@mat.umk.pl>
2025-12-03 10:43:40 +0100

    commit: dodano program.cpp

---
+++ program.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int wartosc;
+	cout << "Wprowadz liczbe: \n";
+	cin >> wartosc;
+	cout << "Wczyana liczba to: " << wartosc << '\n';
+	return 0;
+}
