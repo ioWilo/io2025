73b09524d277824c81f48c56b41512b8e7206991
wilkanowski <wilkanowski@mat.umk.pl>
2025-12-03 10:55:36 +0100

    dodano sprawdzanie czy liczba jest nieujemna

--- program.cpp
+++ program.cpp
@@ -5,6 +5,10 @@ int main() {
 	int wartosc;
 	cout << "Wprowadz liczbe: \n";
 	cin >> wartosc;
+	if(wartosc < 0) {
+		cout << "blad: liczba ujemna\n";
+		return 1;
+	}
 	cout << "Wczyana liczba to: " << wartosc << '\n';
 	return 0;
 }
